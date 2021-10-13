3e672e593649ac11623578c3b8e6967fee109a2a
Tushar Borchate <tushar@fitnessbi.com>
2021-10-13 21:26:42 +0000

    added aws lambda function

---
+++ AWSLambda/Lambda.cs
@@ -0,0 +1,39 @@
+﻿using Amazon.Lambda.DynamoDBEvents;
+using Amazon.Lambda.Serialization.Json;
+using System;
+using System.IO;
+using System.Text;
+
+namespace AWSLambda
+{
+    public class DynamoDBLambda
+    {
+        private static readonly JsonSerializer _jsonSerializer = new JsonSerializer();
+
+        public void ProcessDynamoEvent(DynamoDBEvent dynamoEvent)
+        {
+            Console.WriteLine($"Beginning to process {dynamoEvent.Records.Count} records...");
+
+            foreach (var record in dynamoEvent.Records)
+            {
+                Console.WriteLine($"Event ID: {record.EventID}");
+                Console.WriteLine($"Event Name: {record.EventName}");
+
+                string streamRecordJson = SerializeObject(record.Dynamodb);
+                Console.WriteLine($"DynamoDB Record:");
+                Console.WriteLine(streamRecordJson);
+            }
+
+            Console.WriteLine("Stream processing complete.");
+        }
+
+        private string SerializeObject(object streamRecord)
+        {
+            using (var ms = new MemoryStream())
+            {
+                _jsonSerializer.Serialize(streamRecord, ms);
+                return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+    }
+}
